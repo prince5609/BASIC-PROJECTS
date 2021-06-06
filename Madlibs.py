c615cc1b621c2c090b5a22dc160bc22366ac2675
prince5609 <prince.tyagi98@gmail.com>
2021-06-06 09:37:32 +0530

    Madlibs.....Just For Fun

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+name = str(input("enter your name :"))
+
+age = int(input("what's your age :"))
+
+gender = str(input("are you male or female :"))
+
+hobby = str(input("what's your hobby :"))
+
+aim = str(input("what you want to be in life :"))
+
+result = f"Hey {name} \nyou are {age}\nyour gender is {gender}\nyou like to do {hobby}\nand definitely you will be " \
+         f"good {aim} one day. "
+
+print(result)
