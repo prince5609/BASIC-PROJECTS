2df9a25a9ce1c3ab854db8caabe96552607f2e0e
prince5609 <prince.tyagi98@gmail.com>
2021-06-06 11:32:24 +0530

    Dice Roll

--- Roll_Dice.py
+++ Roll_Dice.py
@@ -4,20 +4,20 @@ import random
 def roll_dice():
     print("Welcome")
     operation = input("Do you want to roll the dice, Y for yes and N for no? :")
-    while True:
-        if operation != "N":
-            if operation == "Y":
-                print(random.randint(1, 6))
-                print(random.randint(1, 6))
-                ask = input("Do you want to roll it again Y for yes &  N for Y? :")
-                if ask == "Y":
-                    continue
-                elif ask == "N":
-                    print("ok! Good Bye")
-                    break
+    while operation != "N" and operation != "n":
+        if operation == "Y" or operation == "y" :
+            print(random.randint(1, 6))
+            print(random.randint(1, 6))
+            ask = input("Do you want to roll it again Y for yes &  N for Y? :")
+            if ask == "Y" or ask == "y":
+                continue
+            elif ask == "N" or ask == "n":
+                break
         else:
-            print("ok! Good Bye")
-            break
+            print("Invalid Input try again")
+            operation = input("Do you want to roll the dice, Y for yes and N for no? :")
+
+    return print("Ok! Good Bye")
 
 
 roll_dice()
